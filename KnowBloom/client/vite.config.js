ab4fae7a70660dbca7a86b3f118c813083f04712
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-12 01:43:31 +0530

    Update vite.config.js

--- KnowBloom/client/vite.config.js
+++ KnowBloom/client/vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
