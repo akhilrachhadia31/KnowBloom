ad588c7d23ec2128131720bdbbdd7816f0bf50f5
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-12 17:07:17 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (res, user, message) => {
+  if (!process.env.SECRET_KEY) {
+    throw new Error("SECRET_KEY environment variable is not set");
+  }
   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
     expiresIn: "1d",
   });
@@ -9,8 +12,8 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
-      secure: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     })
     .json({
