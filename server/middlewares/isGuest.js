ef026cebe2c0dba9e49b98de2e3b7b1541a386ca
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-13 01:36:06 +0530

    Clean up merge conflicts

--- server/middlewares/isGuest.js
+++ server/middlewares/isGuest.js
@@ -1,12 +1,3 @@
-const isGuest = (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (token) {
-    return res.status(403).json({ message: "Already logged in." });
-  }
-
-  next();
-
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -31,7 +22,6 @@ const isGuest = (req, res, next) => {
     });
     next();
   }
->>>>>>> e0bb359b91c4b7263b1c50098be3c68aee652981
 };
 
 export default isGuest;
