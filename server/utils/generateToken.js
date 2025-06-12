d3345e4a1b79fcf3e576fb23a0a697739bb5a3dc
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-12 18:07:45 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -23,7 +23,7 @@ export const generateToken = (res, user, message) => {
     });
 };
 
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
 // export const generateToken = (res, user, message) => {
 //   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
