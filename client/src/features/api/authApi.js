3536d0e3f78576fa069968fa132b36408de98e62
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-12 18:03:34 +0530

    Update authApi.js

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -2,9 +2,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-
-const USER_API =
-  import.meta.env.VITE_USER_API || "/api/v1/user";
+const USER_API = import.meta.env.VITE_API_URL || "https://knowbloom1.onrender.com/api/v1/user";
 
 export const authApi = createApi({
   reducerPath: "authApi",
