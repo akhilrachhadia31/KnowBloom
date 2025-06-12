c9d6114cb96e0e3f6e5802022ed44ae1949500f7
Akhil Rachhadia <202201005@dau.ac.in>
2025-06-12 20:00:15 +0530

    Add reusable CourseSkeleton component

--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -1,7 +1,7 @@
-import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 import Course from "./Course";
 import { useGetPublishedCourseQuery } from "@/features/api/courseApi";
+import CourseSkeleton from "@/components/CourseSkeleton";
 
 const Courses = () => {
   const { data, isLoading, isError } = useGetPublishedCourseQuery();
@@ -28,22 +28,3 @@ const Courses = () => {
 };
 
 export default Courses;
-
-const CourseSkeleton = () => {
-  return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow rounded-lg overflow-hidden">
-      <Skeleton className="w-full h-36" />
-      <div className="px-5 py-4 space-y-3">
-        <Skeleton className="h-6 w-3/4" />
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3">
-            <Skeleton className="h-6 w-6 rounded-full" />
-            <Skeleton className="h-4 w-20" />
-          </div>
-          <Skeleton className="h-4 w-16" />
-        </div>
-        <Skeleton className="h-4 w-1/4" />
-      </div>
-    </div>
-  );
-};
